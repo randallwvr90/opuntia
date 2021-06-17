8fc8a6403a1a3f22f436d143fa1cb127c2c43b84
randallwvr90 <randallwvr90@gmail.com>
2021-06-17 13:09:27 -0400

    Update .gitignore
    Added *.jpg and images/*.jpg to .gitignore file. Changed opuntia script
    to save images to a new images folder.

--- opuntia.py
+++ opuntia.py
@@ -9,6 +9,6 @@ PAUSE_TIME = 6
 thisFrame = 0
 while thisFrame < FRAMES:
     imgNumber = str(thisFrame).zfill(3)
-    os.system("raspistill -o image%s.jpg"%(imgNumber))
+    os.system("raspistill -o images/image%s.jpg"%(imgNumber))
     thisFrame += 1
     time.sleep(PAUSE_TIME - 6) #Takes roughly 6 seconds to take a picture
