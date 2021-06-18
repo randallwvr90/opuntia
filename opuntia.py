05c4cd5d6da764dd3ec88014c191ddcfe6920bee
Randy Weaver <randyweaver@randys-mbp.attlocal.net>
2021-06-18 13:54:39 -0400

    Clean up and add some comments

--- opuntia.py
+++ opuntia.py
@@ -1,14 +1,14 @@
-import os
-import time
 from timeLapseService import TimeLapseService
 
-class TimeLapse():
-    pass
+# drive push -no-prompt <filepath>
+# this is the shell command to push a file to the google drive. I think it might need to be done 
+# with the working directory set to the initialized google drive directory - so that might need
+# to be opuntia/
 
 def main():
     #Constants
-    FRAMES = 5
-    PAUSE_TIME = 6
+    FRAMES = 200
+    PAUSE_TIME = 120
 
     tls = TimeLapseService(pauseTime = PAUSE_TIME, frames = FRAMES)
     tls.mainLoop()
